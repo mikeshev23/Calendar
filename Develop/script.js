c9843113c9a2079c8893051a69b05620ebfd6a7b
mike <mikeshev23@gmail.com>
2020-02-04 16:53:53 -0800

    added JS and more html

--- Develop/script.js
+++ Develop/script.js
@@ -1,25 +1,54 @@
+var currentHour = moment().hours(); //retrives current hour
 
+//set intial time on page load
+timerInterval();
 
+//set intial background colors on page load
+setBackground();
 
+//Function runs to check if attribute "data-time" is <=> current time, if so set the appropriate class based on time.
+function setBackground() {
+    $(".time-block").each(function () {
+        var blockHour = parseInt($(this).data('time'));
 
-function checkTime(i) {
-    if (i < 10) {
-      i = "0" + i;
-    }
-    return i;
-  }
-  
-  function startTime() {
-    var today = new Date();
-    var h = today.getHours();
-    var m = today.getMinutes();
-    var s = today.getSeconds();
-    // add a zero in front of numbers<10
-    m = checkTime(m);
-    s = checkTime(s);
-    document.getElementById('time').innerHTML = h + ":" + m + ":" + s;
-    t = setTimeout(function() {
-      startTime()
-    }, 500);
-  }
-  startTime();
+        if (blockHour < currentHour) {
+            $(this).find('.form-control').removeClass('present future').addClass('past');
+
+        } else if (blockHour === currentHour) {
+            $(this).find('.form-control').removeClass('past future').addClass('present');
+
+        } else if (blockHour > currentHour) {
+            $(this).find('.form-control').removeClass('past present').addClass('future');
+        }
+    });
+};
+
+//Funtion to update jumbotron date/time
+function timerInterval() {
+    $('#time').text(moment().format('LLLL'));
+}
+
+//run setBackground function every 3 seconds
+setInterval(setBackground, 3000);
+
+//run timerInterval function every 1 seconds
+setInterval(timerInterval, 1000);
+
+//Save value of key index & text content to local storage
+$(".saveBtn").on("click", function () {
+    var key = $(this).parent().data('time');
+    var value = $(this).parent().find('.form-control').val();
+    localStorage.setItem(key, value); //save in local storage
+});
+
+//Load saved data from local storage to textarea (was not able to get for loop to work for this)
+$('[data-time="8"]').find("textarea").val(localStorage.getItem("8"));   
+$('[data-time="9"]').find("textarea").val(localStorage.getItem("9"));
+$('[data-time="10"]').find("textarea").val(localStorage.getItem("10"));
+$('[data-time="11"]').find("textarea").val(localStorage.getItem("11"));
+$('[data-time="12"]').find("textarea").val(localStorage.getItem("12"));
+$('[data-time="13"]').find("textarea").val(localStorage.getItem("13"));
+$('[data-time="14"]').find("textarea").val(localStorage.getItem("14"));
+$('[data-time="15"]').find("textarea").val(localStorage.getItem("15"));
+$('[data-time="16"]').find("textarea").val(localStorage.getItem("16"));
+$('[data-time="17"]').find("textarea").val(localStorage.getItem("17"));
