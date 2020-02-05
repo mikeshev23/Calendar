73ffeb124a5685c2b21240946b0e0b922ca5ac30
mike <mikeshev23@gmail.com>
2020-02-04 17:18:20 -0800

    added more JS but didn't get the local storage to workout, used a classmates JS

--- Develop/script.js
+++ Develop/script.js
@@ -3,33 +3,10 @@ var currentHour = moment().hours(); //retrives current hour
 //set intial time on page load
 timerInterval();
 
-//set intial background colors on page load
-setBackground();
-
-//Function runs to check if attribute "data-time" is <=> current time, if so set the appropriate class based on time.
-function setBackground() {
-    $(".time-block").each(function () {
-        var blockHour = parseInt($(this).data('time'));
-
-        if (blockHour < currentHour) {
-            $(this).find('.form-control').removeClass('present future').addClass('past');
-
-        } else if (blockHour === currentHour) {
-            $(this).find('.form-control').removeClass('past future').addClass('present');
-
-        } else if (blockHour > currentHour) {
-            $(this).find('.form-control').removeClass('past present').addClass('future');
-        }
-    });
-};
-
 //Funtion to update jumbotron date/time
 function timerInterval() {
-    $('#time').text(moment().format('LLLL'));
+    $('#currentDay').text(moment().format('LLLL'));
 }
-
-//run setBackground function every 3 seconds
-setInterval(setBackground, 3000);
 
 //run timerInterval function every 1 seconds
 setInterval(timerInterval, 1000);
@@ -41,14 +18,13 @@ $(".saveBtn").on("click", function () {
     localStorage.setItem(key, value); //save in local storage
 });
 
-//Load saved data from local storage to textarea (was not able to get for loop to work for this)
-$('[data-time="8"]').find("textarea").val(localStorage.getItem("8"));   
-$('[data-time="9"]').find("textarea").val(localStorage.getItem("9"));
-$('[data-time="10"]').find("textarea").val(localStorage.getItem("10"));
-$('[data-time="11"]').find("textarea").val(localStorage.getItem("11"));
-$('[data-time="12"]').find("textarea").val(localStorage.getItem("12"));
-$('[data-time="13"]').find("textarea").val(localStorage.getItem("13"));
-$('[data-time="14"]').find("textarea").val(localStorage.getItem("14"));
-$('[data-time="15"]').find("textarea").val(localStorage.getItem("15"));
-$('[data-time="16"]').find("textarea").val(localStorage.getItem("16"));
-$('[data-time="17"]').find("textarea").val(localStorage.getItem("17"));
+//Load saved data from local storage to form-control (was not able to get for loop to work for this)
+$('[data-time="8"]').find("form-control").val(localStorage.getItem("8"));   
+$('[data-time="9"]').find("form-control").val(localStorage.getItem("9"));
+$('[data-time="10"]').find("form-control").val(localStorage.getItem("10"));
+$('[data-time="11"]').find("form-control").val(localStorage.getItem("11"));
+$('[data-time="12"]').find("form-control").val(localStorage.getItem("12"));
+$('[data-time="13"]').find("form-control").val(localStorage.getItem("13"));
+$('[data-time="14"]').find("form-control").val(localStorage.getItem("14"));
+$('[data-time="15"]').find("form-control").val(localStorage.getItem("15"));
+$('[data-time="16"]').find("form-control").val(localStorage.getItem("16"));
